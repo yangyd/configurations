ca14963660b15c41928b19bcea479f8bd05b49c5
Yuedong Yang <yangyd@live.com>
2016-05-05 16:49:53 +0000

    update cmdrc

--- cmdrc.bat
+++ cmdrc.bat
@@ -1,28 +1,22 @@
 
 @echo off
+set PATH=%USERPROFILE%\.local\bin;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;
 
-set PATH=%USERPROFILE%\.local\bin;%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;%Systemroot%\System32\WindowsPowerShell\v1.0\;
-set PATH=%AppData%\Zeon\bin;%PATH%
+set VIM=C:\Users\Yuedong\apps\vim-7.4.1770
 
-set PATH=%JAVA_HOME%\bin;%PATH%
+set PATH=%PATH%;%VIM%\bin;%JAVA_HOME%\bin
 
-set PYTHON_HOME=C:\Program Files\Python 3.5
-set PATH=%PATH%;%PYTHON_HOME%;%PYTHON_HOME%\Scripts
-
-:: set PATH=%PATH%;%SCALA_HOME%\bin
-:: set PATH=%PATH%;%M2_HOME%\bin
-:: set PATH=C:\home\wamp\mingw64\bin;%PATH%
+set PATH=%PATH%;C:\Users\Yuedong\AppData\Local\Programs\Python\Python35-32
+set PATH=%PATH%;C:\Program Files\7-Zip
 
 doskey ll=dir /a $*
 doskey mv=move $*
-doskey cp=xcopy /I /V /K /F /H /J $*
-doskey rm=del /P $*
+doskey cp=xcopy /I /H /V /K /F $*
+doskey rm=del /p $*
+doskey here=explorer .
 doskey cat=type $*
 doskey pwd=cd
-doskey here=explorer .
-doskey ps=tasklist
-doskey sh=start $*
-doskey cygwin=C:\home\wamp\cygwin\Cygwin.bat
+doskey date=cmd /C "date /t && time /t"
 
-:: cd %userprofile%\home
+echo on
 
